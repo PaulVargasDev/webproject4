f0961d505ba3330c70b8269d35694d5a9f012e41
Paul Vargas <paulvargasdev@gmail.com>
2017-10-24 22:34:41 -0400

    Added Days left to Christmas

--- js/time.js
+++ js/time.js
@@ -52,4 +52,15 @@
     refresh();
 
     var interval = setInterval(refresh, 1000);
+
+/*Days left until next Christmas*/
+    today=new Date();
+    var cmas=new Date(today.getFullYear(), 11, 25);
+    if (today.getMonth()==11 && today.getDate()>25) 
+    {
+    cmas.setFullYear(cmas.getFullYear()+1); 
+    }  
+    var one_day=1000*60*60*24;
+    document.getElementById("untilcmas").innerHTML = "<b>Christmas</b> " + "will be in "+Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+    " days!";
 }())
